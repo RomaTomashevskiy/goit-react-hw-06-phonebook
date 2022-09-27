bff2e8023be34d744200b563eba594203490263c
RomaTomashevskiy <romatomashevskiy@ukr.net>
2022-09-28 00:05:48 +0300

    phonebook-redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore , combineReducers} from "@reduxjs/toolkit";
 import { contactSlice } from "./contactSlice";
 
+import {
+    persistStore,
+    persistReducer,
+     FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const rootReducer = combineReducers({
+    contacts: contactSlice.reducer,
+})
+
+const persistedReducer = persistReducer(persistConfig , rootReducer)
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactSlice.reducer,
-    }
+    reducer: persistedReducer,
+     middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-
+export const persistor = persistStore(store)
